94cc698b0809283cc87e1ad35dee1044460d4bef
Anthony Pileggi <adpileggi@gmail.com>
2024-05-22 19:25:01 -0400

    update

--- client/src/pages/Doctor-login.jsx
+++ client/src/pages/Doctor-login.jsx
@@ -1,9 +1,44 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-export default function Tab3() {
-    return (
-      <div>
-        <h1>Tab3</h1>
-      </div>
-    );
-  }
-  
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../utils/mutations";
+
+const Login = (props) => {
+  const [formState, setFormState] = useState({ username: "", password: "" });
+  const [login, { error, data }] = usemutation(LOGIN_USER);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+    setFormState({
+      username: "",
+      password: "",
+    });
+  };
+
+  return (
+    <div>
+      <h1>Tab3</h1>
+    </div>
+  );
+};
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -4,7 +4,7 @@ export const LOGIN_USER = gql`
     mutation login($username: String!, $password: String!){
         login(username: $username, password: $password){
             token
-             user{
+             doctor{
                 _id
                 username
              }
